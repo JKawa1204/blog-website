94ca292a963ec6fc4340cc75b59115294599575e
JKawa1204 <jaykawa1234@gmail.com>
2024-09-20 21:07:42 +0530

    Deployment issues solved

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Specify the output directory, adjust if necessary
+    sourcemap: true, // Useful for debugging, can be turned off in production
+  },
+});
